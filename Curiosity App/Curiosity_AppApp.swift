bc9bfa52ab9b13389b65a77c8ce7c6419583fa28
emilyprietob <emilyprietob@gmail.com>
2026-06-25 11:33:26 -0400

    Initial Commit

---
+++ Curiosity App/Curiosity_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curiosity_AppApp.swift
+//  Curiosity App
+//
+//  Created by Emily Prieto on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Curiosity_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
